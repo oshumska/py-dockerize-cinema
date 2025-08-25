b7d1dfbddffbb3965e44e60883176808f9c97e82
Olha Shumska <olgashumska65@gmail.com>
2025-08-25 14:25:46 +0200

    Solution

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -138,8 +138,8 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = "files/static"
+STATIC_URL = "/static/"
+STATIC_ROOT = "/files/static"
 
 MEDIA_ROOT = "/files/media"
 MEDIA_URL = "/media/"
